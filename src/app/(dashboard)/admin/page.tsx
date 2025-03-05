22d21bb7527f55f64b97e76b6fa5fba67be74881
Pagnol_wesley <emperorb007@gmail.com>
2025-03-05 07:14:00 +0100

    Dashboard ui

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
@@ -18,6 +19,9 @@ const AdminPage = () => {
           {/* COUNT CHARTS  */}
           <div className="w-full  lg:w-1/3 h-[450px]">
             <CountChart />
+          </div>
+          <div className="w-full  lg:w-1/3 h-[450px]">
+            <AttendanceChart />
           </div>
           {/* ATTENDANCE CHARTS  */}
           <div className="w-full  lg:w-2/3 h-[450px]"></div>
